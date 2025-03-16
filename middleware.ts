37a926e8f6efaf5000e87e3e0c665d84023e1224
woleEkanola <woleekanola@gmail.com>
2025-03-16 16:57:12 +0100

    update

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const hostname = request.headers.get('host') || ''
-  
+  console.log('xdddddddddddddddddddddd', hostname)
   if (hostname === 'jessegeorge.greenvites.online') {
     // Redirect root to the event page (optional)
     if (request.nextUrl.pathname === '/') {
