aabfdf4668957b568cbd1712a5a483ca2a554615
woleEkanola <woleekanola@gmail.com>
2025-03-16 17:14:17 +0100

    subdotestmain

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,13 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const hostname = request.headers.get('host') || ''
-  console.log('xdddddddddddddddddddddd', hostname)
-  if (hostname === 'jessegeorge.greenvites.online') {
-    // Redirect root to the event page (optional)
-    if (request.nextUrl.pathname === '/') {
-      return NextResponse.redirect(new URL('/jessegeorge', request.url))
-    }
-  }
+  return NextResponse.redirect(new URL('/jessegeorge', request.url))
+//   if (hostname === 'jessegeorge.greenvites.online') {
+//     // Redirect root to the event page (optional)
+//     if (request.nextUrl.pathname === '/') {
+//       return NextResponse.redirect(new URL('/jessegeorge', request.url))
+//     }
+//   }
 
   return NextResponse.next()
 }
