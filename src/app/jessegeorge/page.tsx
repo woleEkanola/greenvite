fc313b798258c055eddca46628fc8bc90cc8e5c2
woleEkanola <woleekanola@gmail.com>
2025-03-16 16:04:06 +0100

    update

--- src/app/jessegeorge/page.tsx
+++ src/app/jessegeorge/page.tsx
@@ -1,5 +1,17 @@
-import EventPage from './components/InteractiveEventPage'
+import dynamic from 'next/dynamic'
+
+const InteractiveEventPage = dynamic(
+  () => import('./components/InteractiveEventPage'),
+  { 
+    ssr: false,
+    loading: () => (
+      <div className="min-h-screen flex items-center justify-center bg-white">
+        <div className="text-gray-600">Loading invitation...</div>
+      </div>
+    )
+  }
+)
 
 export default function Page() {
-  return <EventPage />
+  return <InteractiveEventPage />
 }
